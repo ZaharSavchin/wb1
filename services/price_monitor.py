46572eb6d4a2eab240e6de8ea5aa1b6d075fbd81
zahar <pasechnik.pchelovod@mail.ru>
2023-05-31 09:21:09 +0300

    loops

--- services/price_monitor.py
+++ services/price_monitor.py
@@ -18,7 +18,7 @@ async def monitoring():
                                                                          f"{list_of_items[0]}")
                             await main_search(list_of_items[0], item_id, user_id)
         loop_counter += 1
-        if loop_counter % 120 == 0 or loop_counter == 0:
+        if loop_counter % 120 == 0 or loop_counter == 1:
             await bot.send_message(chat_id=1042048167, text=f"{loop_counter}")
         await asyncio.sleep(60)
 
