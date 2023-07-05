f9e7ba841a2661bf1f70bc01ae55796520df8e8c
zahar <pasechnik.pchelovod@mail.ru>
2023-07-05 10:28:26 +0300

    bug

--- services/price_monitor.py
+++ services/price_monitor.py
@@ -9,23 +9,26 @@ async def monitoring():
         counter = 1
         for user_id, list_of_items in users_items.copy().items():
             if len(list_of_items[1]) > 0:
-                for item_id, price in list_of_items[1].items():
-                    print(f'{counter}) {item_id}')
-                    # actual_price = await get_price(list_of_items[0], item_id)
-                    # actual_price_float = actual_price.pop()
-                    response = await get_item(currency=list_of_items[0], item_id=item_id)
-                    item_details = await get_item_details(response)
-                    actual_price = float(item_details.get('salePriceU', None) / 100) if item_details.get('salePriceU', None) is not None else None
-                    name = item_details.get('name', None)
-                    if actual_price < price:
-                        try:
-                            sale = price - actual_price
-                            await bot.send_message(chat_id=user_id, text=f"цена товара '{name}' (Артикул: {item_id})"
-                                                                     f" снизилась на {round(sale, 2)} "
-                                                                     f"{list_of_items[0]}")
-                            await main_search(list_of_items[0], item_id, user_id, item_details=item_details)
-                        except Exception as error:
-                            print(error)
+                try:
+                    for item_id, price in list_of_items[1].items():
+                        print(f'{counter}) {item_id}')
+                        # actual_price = await get_price(list_of_items[0], item_id)
+                        # actual_price_float = actual_price.pop()
+                        response = await get_item(currency=list_of_items[0], item_id=item_id)
+                        item_details = await get_item_details(response)
+                        actual_price = float(item_details.get('salePriceU', None) / 100) if item_details.get('salePriceU', None) is not None else None
+                        name = item_details.get('name', None)
+                        if actual_price < price:
+                            try:
+                                sale = price - actual_price
+                                await bot.send_message(chat_id=user_id, text=f"цена товара '{name}' (Артикул: {item_id})"
+                                                                         f" снизилась на {round(sale, 2)} "
+                                                                         f"{list_of_items[0]}")
+                                await main_search(list_of_items[0], item_id, user_id, item_details=item_details)
+                            except Exception as error:
+                                print(error)
+                except Exception as error:
+                    print(error)
             else:
                 print(counter)
             counter += 1
